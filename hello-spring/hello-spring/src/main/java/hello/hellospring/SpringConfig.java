7577f05adfd587dc27e4eb710e401d9590dc5567
Donghyun-34 <akakak413@naver.com>
2021-07-09 23:31:08 +0900

    스프링 공부 5일차
    1. JDBC 연결 공부
    2. 스프링 통합 테스트

--- hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -18,6 +18,11 @@ import org.springframework.context.annotation.Configuration;
  * 1. 정형화된 경우 : Component 스캔 방식
  * 2. 정형화되어 있지 않은 경우 : 코드 선언 방식
  * 3. 정리 : 즉, 추후 변형 될 수도 있는 경우에는 코드 선언 방식을 사용 -> 설정 파일 하나만 수정하면 되기 때문이다.
+
+ * 해당 예제에서는 설계 단계에서는 아직 데이터 저장 개체를 설정하지 못했다는 가정으로 진행되기 때문에
+ * 초기에는 단순 MemoryMember Repository 를 따로 만들어서 사용하다가,
+ * 추후 DB에 연결해주는 방식으로 변경하는 식으로 진행되기 때문에
+ * 변경을 위해서 코드 선언 방식으로 진행.
  */
 
 @Configuration
